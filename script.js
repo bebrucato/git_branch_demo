eba81cb3ab341b71de606951665df923f19360e0
B Brucato <bebrucato@gmail.com>
2021-02-10 11:18:18 -0800

    Java function to html

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 alert("Hello");
+
+var choice = confirm("Are you happy?");
+var choice2 = prompt("Choose R, P, S");
+
+console.log(choice2)
+
+var choices = ["R","P","S",];
+
+var randomIndex = Math.floor(Math.random);
+
