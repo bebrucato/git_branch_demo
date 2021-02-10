4965dbdefce90a771bfcc4e40387519e86075e7d
Jeffery Patterson <jpatterson933@ucla.edu>
2021-02-10 11:42:39 -0800

    added function and if statement

--- script.js
+++ script.js
@@ -3,12 +3,19 @@ alert("Ready to play?");
 var choice = confirm("Rock, Paper or Scissors?");
 var choice2 = prompt("Choose R, P, S");
 
+
+
 console.log(choice2)
 
-var choices = ["R","P","S",];
+var choices = ["R","P","S",]; {
+    function computer_choice() {
+        return choices[Math.floor(Math.random()*choices.length)];
+    }
+};
 
-function computer_choice() {
-    return choices[Math.floor(Math.random()*choices.length)];
-}
+
 console.log(computer_choice());
 
+if (choices === choice2) {
+    console.log("tie");
+  }
