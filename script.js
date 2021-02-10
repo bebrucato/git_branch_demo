da59484cfeae844a3903b031677957e0ae4fa3a2
Jeffery Patterson <jpatterson933@ucla.edu>
2021-02-10 11:47:50 -0800

    added console.log and computer choice

--- script.js
+++ script.js
@@ -7,15 +7,15 @@ var choice2 = prompt("Choose R, P, S");
 
 console.log(choice2)
 
-var choices = ["R","P","S",]; {
-    function computer_choice() {
-        return choices[Math.floor(Math.random()*choices.length)];
-    }
-};
+var choices = ["R","P","S",];
 
+function computer_choice() {
+    return choices[Math.floor(Math.random()*choices.length)];
+}
 
-console.log(computer_choice());
+choice=computer_choice();
+console.log(choice);
 
-if (choices === choice2) {
+if (choice === choice2) {
     console.log("tie");
   }
