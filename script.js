7cfa23eba73fb40d15e0562b7489cf932e067a38
Jeffery Patterson <jpatterson933@ucla.edu>
2021-02-10 12:14:23 -0800

    added code i dont understand

--- script.js
+++ script.js
@@ -1,21 +1,35 @@
-alert("Ready to play?");
-
-var choice = confirm("Rock, Paper or Scissors?");
 var choice2 = prompt("Choose R, P, S");
-
-
 
 console.log(choice2)
 
+//this is the computer 
 var choices = ["R","P","S",];
-
 function computer_choice() {
     return choices[Math.floor(Math.random()*choices.length)];
 }
 
+console.log(computer_choice());
+
 choice=computer_choice();
-console.log(choice);
 
 if (choice === choice2) {
     console.log("tie");
-  }
+    }
+if (choice["R"] !== choice2["S"]) {
+      console.log("You Lose");
+    }
+if (choice["P"] === choice2["R"]) {
+        console.log("You Lose");
+    }
+if (choice["S"] === choice2["P"]) {
+        console.log("You Lose");
+    }
+if (choice["S"] === choice2["R"]) {
+        console.log("You win");
+    }
+if (choice["R"] === choice2["P"]) {
+        console.log("You win");
+    }
+if (choice["P"] === choice2["S"]) {
+        console.log("You win");
+}
